05d2624a5bf1af095c726a986743f9588106ddde
j-moreno-c-r <joaomcr@proton.me>
2024-12-06 17:58:46 -0300

    the fucking 2000 childs

--- balance_draft/main.py
+++ balance_draft/main.py
@@ -7,6 +7,10 @@ from childs_priv_keys import get_wallet_privs
 #wpkh(tprv8ZgxMBicQKsPdUpU7MYiBXtJ2Ss5h2hCjgra8YqdR1dvMWTCMHRmEtUVxp3GGKofZ6zAZNU1E5CkAB2P1QXFECC4QMsUDR1Gpe8zBXdWJm9/84h/1h/0h/0/*)#uadxj6umj
 
 
+BIP32_HARDENED = 0x80000000
+SECP256K1_ORDER = int('FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141', 16)
+SECP256K1_G = int('79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798', 16)
+
 if __name__ == "__main__":
     all_bytes=base58_decode("tprv8ZgxMBicQKsPdUpU7MYiBXtJ2Ss5h2hCjgra8YqdR1dvMWTCMHRmEtUVxp3GGKofZ6zAZNU1E5CkAB2P1QXFECC4QMsUDR1Gpe8zBXdWJm9")
     #print(len(all_bytes))
@@ -22,8 +26,16 @@ if __name__ == "__main__":
     
     parent_private_key = bytes.fromhex(main_dict["key_data"]) 
     parent_chain_code = bytes.fromhex(main_dict["chain_code"])
-    print(derive_priv_child(parent_private_key,parent_chain_code,index=0,hardened=True))
+    #print(derive_priv_child(parent_private_key,parent_chain_code,index=0,hardened=True))
+    
+    
+    derivation_path = [
+        (84 | BIP32_HARDENED, True),
+        (0 | BIP32_HARDENED, True),
+        (0 | BIP32_HARDENED, True)
+    ]
 
+    print(get_wallet_privs(parent_private_key,parent_chain_code,derivation_path))
 
 
 
